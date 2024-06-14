411f1e1242700a5e17d03bfe67db9472e82cb9b0
IsPekToP <inspektop1@gmail.com>
2024-06-14 13:58:45 +0200

    Подключил jquery через CDN от гугла

--- 14main2_1/js/index.js
+++ 14main2_1/js/index.js
@@ -1,3 +1,12 @@
 //14main2_1
 //Подключение jQuery к проекту
 
+//1й вариан: на сайте jquery.com скачать его(затем создать папку в проэкте
+//и затем подключить в теге script <script src="назв. папки"></script>)
+//не самый лучший способ(прийдется всегда скачивать эту б-ку) лучше 2м
+
+//2й вариант:через CDN от GOOgLe
+//прописать в гугле:google api jquery>> Hosted Libraries>> и скопировать в index.html 
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+//в середине тега <body>
+//подключать надо вверху,перед его написания
