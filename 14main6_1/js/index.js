eeed517da08f6acab816cdcc81e2cf12ed4fe706
IsPekToP <inspektop1@gmail.com>
2024-06-26 02:02:40 +0200

    шапка следит за скролом

--- 14main6_1/js/index.js
+++ 14main6_1/js/index.js
@@ -8,5 +8,28 @@
 $(document).scroll(function(){
     //если ширина документа меньше 1024px,то фиксировать шапку не будем
     if($(document).width() < 1024)
-        return false
+        return false;
+    //узнаем сколько мы уже проскролили scrollTop - в пикселях
+    //$('.ful-page' - обращаемся к нашему обьекту с главной картинкой
+    if($(document).scrollTop() > $('.full-page').height()/2) 
+        $("header").addClass("fixed");
+        //беру header и добавляю к нему класс fixed
+    else
+        //удаляем класс
+        $("header").removeClass("fixed");    
 });
+
+//вывод прокрутки в еонсоль(не онлайн)
+// var scrol = $(document).scrollTop();
+// console.log(scrol);
+
+//повтор кода без коментов
+
+// $(document).scroll(function(){
+//     if($(document).width() < 1024)
+//         return false;
+//     if($(document).scrollTop() > $('.full-page').height() / 2)
+//         $("header").addClass("fixed");
+//     else
+//         $("header").removeClass("fixed");
+// });
